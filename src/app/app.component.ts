936831dca1f7376db8a88d0e8ae1e6a97ec9369c
Sondre Edvardsen <sondre@mystore.no>
2016-06-28 16:20:50 +0200

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MdButton } from '@angular2-material/button';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
+import { MD_TABS_DIRECTIVES } from '@angular2-material/tabs';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+import { CartComponent } from './cart/cart.component';
+import { ProductsService } from './products.service';
+import { CategoryComponent } from './category/category.component';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+      MdButton,
+      MD_CARD_DIRECTIVES,
+      MD_LIST_DIRECTIVES,
+      MD_SIDENAV_DIRECTIVES,
+      MD_TOOLBAR_DIRECTIVES,
+      MD_TABS_DIRECTIVES,
+      MdIcon,
+      CartComponent,
+      ROUTER_DIRECTIVES
+  ],
+  providers: [MdIconRegistry, ProductsService]
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+
+    constructor(private productsservice: ProductsService) {}
+
+    title = 'Angular2 test!';
+    private categories: any;
+
+    ngOnInit() {
+        //console.log(this.categories);
+        this.productsservice.getCategories()
+            .subscribe(
+                data => this.categories = data
+            )
+        ;
+    }
+
 }
--- src/system-config.ts
+++ src/system-config.ts
@@ -7,11 +7,34 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+    '@angular2-material': 'vendor/@angular2-material'
 };
 
 /** User packages configuration. */
 const packages: any = {
 };
+
+const materialPkgs:string[] = [
+    'core',
+    'button',
+    'card',
+    'checkbox',
+    'grid-list',
+    'icon',
+    'input',
+    'list',
+    'progress-bar',
+    'progress-circle',
+    'radio',
+    'sidenav',
+    'slide-toggle',
+    'tabs',
+    'toolbar'
+];
+
+materialPkgs.forEach((pkg) => {
+  packages[`@angular2-material/${pkg}`] = {main: `${pkg}.js`};
+});
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
@@ -33,6 +56,9 @@ const barrels: string[] = [
   // App specific barrels.
   'app',
   'app/shared',
+  'app/cart',
+  'app/category/category',
+  'app/category',
   /** @cli-barrel */
 ];
 
