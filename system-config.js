f20909d87754c1fdb49cd73a018c8b1de8ea8aa9
Sondre Edvardsen <sondre@mystore.no>
2016-06-28 16:42:23 +0200

    new gh-pages version

---
+++ system-config.js
@@ -0,0 +1,70 @@
+// SystemJS configuration file, see links for more information
+// https://github.com/systemjs/systemjs
+// https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {
+    '@angular2-material': 'vendor/@angular2-material'
+};
+/** User packages configuration. */
+var packages = {};
+var materialPkgs = [
+    'core',
+    'button',
+    'card',
+    'checkbox',
+    'grid-list',
+    'icon',
+    'input',
+    'list',
+    'progress-bar',
+    'progress-circle',
+    'radio',
+    'sidenav',
+    'slide-toggle',
+    'tabs',
+    'toolbar'
+];
+materialPkgs.forEach(function (pkg) {
+    packages[("@angular2-material/" + pkg)] = { main: pkg + ".js" };
+});
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/cart',
+    'app/category/category',
+    'app/category',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
